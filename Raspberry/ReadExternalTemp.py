a8ff3d95e8a8a64acc54169750820cc2d3e0b8f5
jarim6 <jari.mutanen@gmail.com>
2020-02-14 18:50:41 +0200

    Tunti 2 python filu pilveen cputemp add

--- Raspberry/ReadExternalTemp.py
+++ Raspberry/ReadExternalTemp.py
@@ -51,27 +51,3 @@ def pilveen():
   except:
    print("connection failed")
   break
-
-# Kerropa Raspin prossun lampotila Celsiuksina
-def thermometer():
-    while True:
-        #time.sleep(15)
-        # Laske Raspin prossun lampotila Celsiuksina
-        cputempLassi = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3 # Lue lampotila
-        params = urlencode({'field1': cputempLassi, 'key':key })
-        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = HTTPConnection("api.thingspeak.com:80")
-        try:
-            conn.request("POST", "/update", params, headers)
-            response = conn.getresponse()
-            print(cputempLassi)
-            print(response.status, response.reason)
-            data = response.read()
-            conn.close()
-            time.sleep(sleep)
-        except:
-            print("connection failed")
-        break
-if __name__ == "__main__":
-        while True:
-                thermometer()
